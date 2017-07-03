a9b6d3297ac8107c6fcd376e1045e61d6c06cc1f
Gabriele Luz <gabrieleluz@users.noreply.github.com>
2017-07-03 02:37:07 -0300

    altera model e controller de usuario

--- controllers/user_controller.php
+++ controllers/user_controller.php
@@ -3,9 +3,8 @@
     require_once('../models/user.php');
 
     class User_controller extends \Controller\Base {
-        public $fillneeded = ['processo' => 'processo', 'nome' => 'nome', 'rua' => 'rua', 'bairro' => 'bairro', 'numero' => 'numero', 'cidade' => 'cidade','complemento' => 'complemento','endereco_visita' => 'endereco_visita','ponto_referencia' => 'ponto_referencia','telefone' => 'telefone','turno_visita' => 'turno_visita','estado_civil' => 'estado_civil', 'filhos' => 'filhos', 'filhos_com_agressor' => 'filhos_com_agressor', 'escolaridade' => 'escolaridade', 'etnia' => 'etnia', 'idade' => 'idade', 'religiao' => 'religiao', 'profissao' => 'profissao', 'trabalha' => 'trabalha', 'ultimo_emprego' => 'ultimo_emprego', 'local_trabalho' => 'local_trabalho', 'renda' => 'renda', 'responsavel_sustento' => 'responsavel_sustento'];
+        public $fillneeded = ['tipo' => 'tipo', 'login' => 'login', 'senha' => 'senha', 'nome' => 'nome'];
         public $location = '../views/users';
-
     }
 
 
@@ -13,8 +12,3 @@
     $obj = new User_controller();
     $users = $obj->loadAll();
     $user = $obj->one();
-
-
-
-
-    // ,'rua' => 'rua','bairro' => 'bairro','numero' => 'numero','cidade' => 'cidade','complemento' => 'complemento','endereco_visita' => 'endereco_visita','ponto_referencia' => 'ponto_referencia','telefone' => 'telefone','turno_visita' => 'turno_visita','estado_civil' => 'estado_civil', 'filhos' => 'filhos', 'filhos_com_agressor' => 'filhos_com_agressor', 'escolaridade' => 'escolaridade', 'etnia' => 'etnia', 'idade' => 'idade', 'religiao' => 'religiao', 'profissao' => 'profissao', 'trabalha' => 'trabalha', 'ultimo_emprego' => 'ultimo_emprego', 'local_trabalho' => 'local_trabalho', 'renda' => 'renda', 'responsavel_sustento' => 'responsavel_sustento' 
--- models/user.php
+++ models/user.php
@@ -2,9 +2,6 @@
     require_once('../heart/model/base.php');
 
     class User extends \Model\Base {
-        public $fillable = ['processo', 'nome', 'rua', 'bairro', 'numero', 'cidade', 'complemento', 'endereco_visita', 'ponto_referencia', 'telefone', 'turno_visita', 'estado_civil', 'filhos', 'filhos_com_agressor', 'escolaridade', 'etnia', 'idade', 'religiao', 'profissao', 'trabalha', 'ultimo_emprego', 'local_trabalho', 'renda', 'responsavel_sustento' ];
+        public $fillable = ['tipo', 'login', 'senha', 'nome'];
     }
 ?>
-
-
-<!-- 'rua', 'bairro', 'numero', 'cidade', 'complemento', 'endereco_visita', 'ponto_referencia', 'telefone', 'turno_visita', 'estado_civil', 'filhos', 'filhos_com_agressor', 'escolaridade', 'etnia', 'idade', 'religiao', 'profissao', 'trabalha', 'ultimo_emprego', 'local_trabalho', 'renda', 'responsavel_sustento' -->
